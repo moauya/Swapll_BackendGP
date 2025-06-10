c0f6ea26b6c58a4fe82af5bb2e81fffadd6938aa
moauyabdour <moauya4455@gmail.com>
2025-06-11 01:39:06 +0300

    edit the review to accept one review to an offer by a user

--- src/main/java/com/swapll/gradu/repository/ReviewRepository.java
+++ src/main/java/com/swapll/gradu/repository/ReviewRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
     List<Review> findAllByOffer_Id(int offerId);
+    boolean existsByUserIdAndOfferId(int userId, int offerId);
 }
